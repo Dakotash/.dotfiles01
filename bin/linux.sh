de56df605c7af46571b1c78ca55e7084a0da1391
Dakota Hawkins <Dakotash@email.sc.edu>
2022-11-21 23:10:15 -0500

    Created linux shell setup scripts.

--- bin/linux.sh
+++ bin/linux.sh
@@ -4,16 +4,16 @@
 if [[ $uname  = 'Linux' ]]; then #checksif the username is the string linux 
 echo " This is a test"
 else
-	echo "ERROR" &> linuxsetup.log #redirects the error message tothis file
+	echo "ERROR" >> linuxsetup.log #redirects the error message tothis file
 	exit
 	
 fi
 
-if [ -f ".vimrc" ];then
+if [ -f ".vimrc" ];then #This checks if ".vimrc" exists
 	mv .vimrc .bup_vimrc # renames the files
 	echo "the current .vimrc file was changed to ‘.bup vimrc’." >> linuxsetup.log
 fi
 
 vimrc > ~/.vimrc #overwrites the files
 
-echo "source ∼/.dotfiles/etc/bashrc custom" >> .bashrc #adds the stringto the end of the file
+echo "source ∼/.dotfiles/etc/bashrc custom" >> .bashrc #adds the string to the end of the file
